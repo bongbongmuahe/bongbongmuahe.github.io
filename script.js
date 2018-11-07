48caeed13e310ab2ada9126b303c19a6ed7c5c26
bongbongmuahe <44745193+bongbongmuahe@users.noreply.github.com>
2018-11-07 23:33:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function timMinMax(ds)
 {
-	n=ds.length;
-	for(i = 0;i<n;i++)
-	{
-		ds = ds.replace("  "," ");
-	}
-	n=ds.length;
-	for(i = 0;i<n;i++)
-	{
-		ds = ds.replace(" ",",");
-	}
-	txtDaySo.value = ds;
-	n=ds.length;
-	var list = ds.split(",")
+	// n=ds.length;
+	// for(i = 0;i<n;i++)
+	// {
+		// ds = ds.replace("  "," ");
+	// }
+	// n=ds.length;
+	// for(i = 0;i<n;i++)
+	// {
+		// ds = ds.replace(" ",",");
+	// }
+	// txtDaySo.value = ds;
+	// n=ds.length;
+	var list = ds.split(" ")
 	min = Number(list[0]);
 	for (i=0;i<list.length;i++)
 	{
