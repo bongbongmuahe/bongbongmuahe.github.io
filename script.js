57ecddd780737e29582a7024aa6ed14faf67109a
bongbongmuahe <44745193+bongbongmuahe@users.noreply.github.com>
2018-11-07 23:29:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,22 +3,21 @@ function timMinMax(ds)
 	n=ds.length;
 	for(i = 0;i<n;i++)
 	{
-		ds = ds.replace(" ","");
+		ds = ds.replace("  "," ");
+	}
+	n=ds.length;
+	for(i = 0;i<n;i++)
+	{
+		ds = ds.replace(" ",",");
 	}
 	txtDaySo.value = ds;
 	n=ds.length;
-	min = Number(ds[0]);
-	for(i = 0;i<n;i++)
+	var list = ds.split(",")
+	min = Number(list[0]);
+	for (i=0;i<list.length;i++)
 	{
-		if (Number(ds[i])<min)
-			min = Number(ds[i]);
+		if (min>Number(list[i]))
+			min = Number(list[i]);
 	}
 	txtMin.value = min;
-	max = Number(ds[0]);
-	for(i = 0;i<n;i++)
-	{
-		if (Number(ds[i])>max)
-			max = Number(ds[i]);
-	}
-	txtMax.value = max;
 }
